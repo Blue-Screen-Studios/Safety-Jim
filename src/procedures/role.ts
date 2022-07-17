3d1778a434b7d0538c24d3190d4faeddfd65708a
IBXCODECAT <nathan.schmitt7@gmail.com>
2022-07-17 15:26:22 -0500

    Working Roles System!

--- src/procedures/role.ts
+++ src/procedures/role.ts
@@ -1,4 +1,4 @@
-import { APIErrors, Client, Guild, GuildMember, Interaction, Permissions, ReactionCollector, Role, User } from "discord.js";
+import { Client, Guild, GuildMember, Interaction, Permissions, Role, RoleResolvable, User } from "discord.js";
 
 export async function Run(client: Client, interaction: Interaction)
 {
@@ -19,64 +19,69 @@ export async function Run(client: Client, interaction: Interaction)
 
         const guild: Guild = interaction.guild!;
         const action: string = interaction.options.getString("action")!;
-        const role: Role = interaction.options.getRole("role")!;
-        const user: User = interaction.options.getUser("user")!;
+        const role: any  = interaction.options.getRole("role")!;
+        const user: User = interaction.options.getUser("member")!;
 
+            
         const member: GuildMember = guild.members.cache.get(user.id)!;
 
-        //If we are trying to modify our own role, reply with a message saying we can't and return...
-        if(client.user?.id === member.id && action != "has")
-        {
-            await interaction.editReply({
-                content: "Woah there! You must modify my own roles manually..."
-            })
-
-            return;
-        }
-
-        //If we are trying to modify a users role with a role managed by an integration, reply with a message saying we can't and return...
-        if(role.managed && action != "has")
-        {
-            await interaction.editReply({
-                content: "This role is managed by an another integration, and therefore can not be added, removed, or deleted!"
-            })
-
-            return;
-        }
-
-        switch(action)
-        {
-            case "add":
-                await member.roles.add(role);
-                
-                interaction.reply({
-                    content: `I have added the <@&${role.id}> role to <@${user.id}>`
-                })
-
-                return;
-            
-            case "remove":
-                await member.roles.remove(role);
-
-                interaction.reply({
-                    content: `I have removed the <@&${role.id}> role to <@${user.id}>`
-                })
-
-                return;
-            
-            default "has":
-                await member.roles.cache.some(r => r === role);
-        }
-            try
-            {
-                
-            }
-            catch
+        try
+        { 
+            //If we are trying to modify a users role with a role managed by an integration, reply with a message saying we can't and return...
+            if(role.managed && action != "has")
             {
                 await interaction.editReply({
-                    content: "I do not have permission to do that... :(\nRequired Permissions: `MANAGE_ROLES`\nRequired Role Heirchy Level: `Above Target Mentionable`"
-                });
+                    content: "This role is managed by another integration, and therefore can not be added, removed, or deleted!"
+                })
+
+                return;
             }
+
+            switch(action)
+            {
+                case "add":
+                    await member.roles.add(role);
+                    
+                    interaction.editReply({
+                        content: `I have added the <@&${role.id}> role to <@${user.id}>`
+                    })
+
+                    return;
+                
+                case "remove":
+                    await member.roles.remove(role);
+
+                    interaction.editReply({
+                        content: `I have removed the <@&${role.id}> role from <@${user.id}>`
+                    })
+
+                    return;
+                
+                case "has":
+                    
+                    
+                    if(member.roles.cache.some(r => r === role) == true)
+                    {    
+                        interaction.editReply({
+                            content: `<@${user.id}> has the role <@&${role.id}>`
+                        })
+                    }
+                    else
+                    {
+                        interaction.editReply({
+                            content: `<@${user.id}> does not have the role <@&${role.id}>`
+                        })
+                    }
+
+                return;
+            }
+        }
+        catch(ex)
+        {
+            console.log(ex);
+            await interaction.editReply({
+                content: "I do not have permission to do that... :(\nRequired Permissions: `MANAGE_ROLES`\nRequired Role Heirchy Level: `Above Target Mentionable`"
+            });
         }
     }
 }
